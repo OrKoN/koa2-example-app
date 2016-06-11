2d86ab3bb9f3bb88fa86b84e72db752fa1c0f388
OrKoN <alexei.rudenko@gmail.com>
2016-06-11 12:48:42 +0200

    Exclude build folder

--- build/server.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var _app = require('./app');
-
-var _app2 = _interopRequireDefault(_app);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-_app2.default.listen(4000);
